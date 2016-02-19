28699c6cf7d1616cc10105468c28cbc3d4a2799c
Ted Zilist <tzilist@gmail.com>
2016-02-19 14:45:07 -0800

    finished zip functionality

--- server/server.js
+++ server/server.js
@@ -2,29 +2,18 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bundler = require('../bundler/bundler.js');
-const serveStatic = require('serve-static');
-const archiver = require('archiver')
+var EasyZip = require('easy-zip').EasyZip;
 const app = express();
 
 
 app.use(express.static('../'));
 app.use('/download', bundler.bundle);
-var zipLogs = function(working_directory) {
-  var fs = require('fs');
-  var path = require('path');
-  var output = fs.createWriteStream(path.join(working_directory, 'logs.zip'));
-  var archiver =  require('archiver');
-  var zipArchive = archiver('zip');
-
-  zipArchive.pipe(output);
-  zipArchive.bulk([{src: [path.join(working_directory, '*.log')],  expand: true}]);
-  zipArchive.finalize(function(err, bytes) {
-      if (err)
-          throw err;
-
-  console.log('done:', base, bytes);
+app.get('/download', (req, res) => {
+  var zip = new EasyZip();
+  zip.zipFolder(path.join(__dirname,'./../templates'), () => {
+    zip.writeToResponse(res,'download');
   });
-}
- });
+});
+
 
 app.listen(3000);
